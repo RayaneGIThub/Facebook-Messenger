b8e20906d8541ddcf2ed487f2f91341f8b486c8c
RayaneGIThub <rayane.ghazal@student.uclouvain.be>
2023-03-23 23:21:40 +0100

    le script affiche mtn les grpahiques qui montre le nombre de chaque protocol utilisé

--- capture_and_scipt/script_pyshark.py
+++ capture_and_scipt/script_pyshark.py
@@ -8,10 +8,23 @@ Analyses=["Send_message.pcap", "Open_app.pcap", "No_pick_up.pcap"]
 for Analyse in Analyses:
     cap = pyshark.FileCapture( Analyse, only_summaries=True)
     cap.load_packets()
+
     protocolList=[]
+
     print(len(cap))
+
     for packet in cap:
         line = str(packet)
         lineSep = line.split(" ")
         protocolList.append(lineSep[4])
+
     print(protocolList)
+
+    counter = collections.Counter(protocolList)
+    plt.style.use('ggplot')
+    ypos = np.arange(len(list(counter.keys())))
+    plt.bar(ypos,list(counter.values()), align='center',alpha = 0.5,color = ['b','r','g','c','m'])
+    plt.xticks(ypos,list(counter.keys()))
+    plt.ylabel("frequency")
+    plt.xlabel("protcol")
+    plt.show()
